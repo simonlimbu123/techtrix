4602d84964bb9e328f46fec98f53f80194ecd1f3
simonlimbu <simonlimbudope123@gmail.com>
2026-07-09 07:15:55 +0545

    home-update

--- src/pages/techTrixHome.jsx
+++ src/pages/techTrixHome.jsx
@@ -544,7 +544,7 @@ export default function TechTrixNepal() {
               <div key={e.id} className="upcoming-event">
                 <div className="upcoming-event-image">
                   <img
-                  className="upcoming-event-img"
+                    className="upcoming-event-img"
                     src={e.photo}
                     alt=""
                     onClick={() => setSelectedImage(e.photo)}
@@ -699,6 +699,7 @@ export default function TechTrixNepal() {
 
 function ProductOrbit({ open, onToggle }) {
   const positions = [{ x: 0, y: -120 }, { x: -110, y: -60 }, { x: 110, y: -60 }, { x: -110, y: 60 }, { x: 110, y: 60 }, { x: 0, y: 120 }];
+  const positionsMobile = [{ x: 0, y: -90 }, { x: -90, y: -30 }, { x: 90, y: -30 }, { x: -80, y: 50 }, { x: 80, y: 50 }, { x: 0, y: 90 }];
   return (
     <div className="orbit-container">
       <div className="pulse-ring" /><div className="pulse-ring2" />
@@ -706,8 +707,16 @@ function ProductOrbit({ open, onToggle }) {
         {open ? <FaLockOpen className="lock-icon" size={40} /> : <FaLock className="lock-icon" size={40} />}
       </div>
       {open && positions.map((pos, i) => (
-        <Link to={recentEvents[i].url} key={i} className="orbit-icon" style={{ left: `calc(50% + ${pos.x}px)`, top: `calc(50% + ${pos.y}px)`, animationDelay: `${i * 0.07}s`, opacity: 0, color: recentEvents[i].color, border: `2px solid ${recentEvents[i].color}33` }}>
-          {<img style={{ borderRadius: "50%", width: "68px", height: "68px", border: `2px solid ${recentEvents[i].color}` }} src={recentEvents[i].logo} />}
+        <>
+          <Link to={recentEvents[i].url} key={i} className="orbit-icon" style={{ left: `calc(50% + ${pos.x}px)`, top: `calc(50% + ${pos.y}px)`, animationDelay: `${i * 0.07}s`, opacity: 0, color: recentEvents[i].color, border: `2px solid ${recentEvents[i].color}33` }}>
+            {<img style={{ borderRadius: "50%", width: "68px", height: "68px", border: `2px solid ${recentEvents[i].color}` }} src={recentEvents[i].logo} />}
+          </Link>
+        </>
+      ))
+      }
+      {open && positionsMobile.map((pos, i) => (
+        <Link to={recentEvents[i].url} key={i} className="orbit-icon-mobile" style={{ left: `calc(50% + ${pos.x}px)`, top: `calc(50% + ${pos.y}px)`, animationDelay: `${i * 0.07}s`, opacity: 0, color: recentEvents[i].color, border: `2px solid ${recentEvents[i].color}33` }}>
+          {<img style={{ borderRadius: "50%", width: "48px", height: "48px", border: `2px solid ${recentEvents[i].color}` }} src={recentEvents[i].logo} />}
         </Link>
       ))
       }
